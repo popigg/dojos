9abc05944fa149d6813d106ae5b47edb8854f7c8
popigg <popigg@gmail.com>
2012-11-26 00:09:46 +0000

    stike test

--- bowlingcalculator/BowlingCalculator.class.php
+++ bowlingcalculator/BowlingCalculator.class.php
@@ -3,20 +3,44 @@
 	*  Class to calculate the score of n bowling frames
 	*/
 	class BowlingCalculator {
-		
-		function bowling_score_calculator ($score)
-		{
-			$result = 0;
-			$i = 0;
 
-			while ($i < strlen($score)) { 				
-				if ($i > 0 && ($score[$i-1] + $score[$i] == 10)) {					
-					$score[$i+1] = 2 * $score[$i+1];					
+		private $score;
+		private $rolls = array();
+		private $current_roll = 0;
+
+		function roll($pins) 
+		{
+			$this->score += $pins;
+			$this->rolls[$this->current_roll++] = $pins;
+		}
+
+		function is_spare($frame_index)
+		{
+			return $this->rolls[$frame_index] + $this->rolls[$frame_index+1] == 10;
+		}
+		
+		function score()
+		{			
+			$this->score = 0;	
+			$frame_index = 0; 
+			for ($frame=0; $frame < 10; $frame++) { 
+				if ($this->rolls[$frame_index] == 10) // strike
+				{
+					$this->score += 10 + $this->rolls[$frame_index+1] + $this->rolls[$frame_index+2];
+					$frame_index++;
+				}	
+				else if ($this->is_spare($frame_index)) // spare 
+				{
+					$this->score += 10 + $this->rolls[$frame_index+2];					
+					$frame_index+= 2;
+				} 
+				else 
+				{
+					$this->score += $this->rolls[$frame_index] + $this->rolls[$frame_index+1];						
+					$frame_index+= 2;
 				}
-				$result += $score[$i];						
-				$i++;				
-			}
-			return $result;
+			}			
+			return $this->score;
 		}
 	}
 ?>
--- bowlingcalculator/BowlingCalculatorTest.class.php
+++ bowlingcalculator/BowlingCalculatorTest.class.php
@@ -16,18 +16,47 @@
 	        // your code here
 	    }
 
-	    function testSimple() {
-	    	$this->assertEquals(63, $this->bowlingCalculator->bowling_score_calculator('31415390107133238009'),
-	    	'Not equal variables');
+	    private function roll_many($counter, $pins)
+	    {
+	    	for ($i=0; $i < $counter; $i++) { 
+	    		$this->bowlingCalculator->roll($pins);	    			
+	    	}
 	    }
 
-	    function testSpare() {
-	    	$this->assertEquals(73, $this->bowlingCalculator->bowling_score_calculator('91415390107133238009'), 
-			'Not equal variables');
+	    private function roll_spare()
+	    {
+	    	$this->bowlingCalculator->roll(5);
+	    	$this->bowlingCalculator->roll(5);
 	    }
 
-	    function testStrikes() {
-	    	
+	    function testZero() 
+	    {
+	    	$this->roll_many(20, 0);	
+	    	$this->assertEquals(0, $this->bowlingCalculator->score(), 'Error in Zeros');
+	    }
+
+	    function testAllOnes()
+	    {	    	
+	    	$this->roll_many(20, 1);
+	    	$this->assertEquals(20, $this->bowlingCalculator->score(), 'Error in All Ones');
+	    }
+
+	    function testSpare()
+	    {
+	    	$this->roll_spare();
+	    	$this->bowlingCalculator->roll(3);
+	    	$this->roll_many(17, 0);
+	    	$this->assertEquals(16, $this->bowlingCalculator->score(), 'Error in Spare');
+
+	    }
+
+	    function testStrike()
+	    {
+	    	$this->bowlingCalculator->roll(10); // Strike
+	    	$this->bowlingCalculator->roll(3);
+	    	$this->bowlingCalculator->roll(4);
+	    	$this->roll_many(17, 0);
+	    	$this->assertEquals(24, $this->bowlingCalculator->score(), 'Error in Strike');
 	    }
 	}
 	
